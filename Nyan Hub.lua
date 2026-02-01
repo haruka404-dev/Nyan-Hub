35462e7bc985eeb0d657b59fceb183c46e9e0b2d
haruka404-dev <haruka.gpk@gmail.com>
2026-02-01 19:51:01 +0700

    Update Nyan Hub configuration and assets

--- Nyan Hub.lua
+++ Nyan Hub.lua
@@ -3,14 +3,14 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
        local Window = MakeWindow({
          Hub = {
          Title = "Nyan Hub",
-         Animation = "By : Nyan Hub"
+         Animation = "Script By : Nyan Hub"
          },
         Key = {
-        KeySystem = false,
+        KeySystem = true,
         Title = "Key System",
         Description = "",
         KeyLink = "",
-        Keys = {"1234"},
+        Keys = {"NYAN"},
         Notifi = {
         Notifications = true,
         CorrectKey = "Running the Script...",
@@ -21,8 +21,8 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
   })
 
        MinimizeButton({
-       Image = "http://www.roblox.com/asset/?id=118045102514600",
-       Size = {60, 60},
+       Image = "http://www.roblox.com/asset/?id=99781525528304",
+       Size = {20, 20},
        Color = Color3.fromRGB(10, 10, 10),
        Corner = true,
        Stroke = false,
