11b74c8e56a5829d7fa3e19a32b7a4903cf5aa96
haruka404-dev <haruka.gpk@gmail.com>
2026-02-02 12:23:50 +0700

    Update URL for Nyan Hub script

--- Nyan Hub.lua
+++ Nyan Hub.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/haruka404-dev/Nyan-Hub/refs/heads/main/Nyan%20Hub%20Menu.lua")))()
 
        local Window = MakeWindow({
          Hub = {
