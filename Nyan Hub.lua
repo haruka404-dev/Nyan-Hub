dfa5e4a56dc9b458b172ba8f8cd61c02e5f37cee
haruka404-dev <haruka.gpk@gmail.com>
2026-02-02 07:23:07 +0700

    Add Andepzai Hub button to Nyan Hub

--- Nyan Hub.lua
+++ Nyan Hub.lua
@@ -65,3 +65,10 @@ AddButton(Tab1o, {
 	  loadstring(game:HttpGet("https://raw.githubusercontent.com/luacoder-byte/luacoder/refs/heads/main/RedzHub.lua"))()
   end
   })
+
+AddButton(Tab1o, {
+     Name = "Andepzai Hub",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/AnDepZaiHub/AnDepZaiHubBeta/refs/heads/main/AnDepZaiHubBeta.lua"))()
+  end
+  })
